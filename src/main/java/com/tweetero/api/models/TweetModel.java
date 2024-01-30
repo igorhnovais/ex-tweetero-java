e3452c0a2e12988f087d748c806e3eb4e1e909b8
Igor Novais <igorhnovaisdriven@gmail.com>
2024-01-30 18:01:07 -0300

    feat: tables db

---
+++ src/main/java/com/tweetero/api/models/TweetModel.java
@@ -0,0 +1,33 @@
+package com.tweetero.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tweets")
+
+public class TweetModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(length = 280, nullable = false)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private UserModel user;
+}
